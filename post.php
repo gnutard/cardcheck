d103e03a034a4e35f96dd9bfcd952ea5f535a40c
gnutard <166033088+gnutard@users.noreply.github.com>
2024-07-24 10:46:57 +0000

    Create post.php

---
+++ post.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<p>You're lucky, we didn't find it. Your card details:</p>
+<?php echo $_POST["name"]; ?>
+<?php echo $_POST["exp"]; ?>
+<?php echo $_POST["ccnumber"]; ?>
+<?php echo $_POST["ccv"]; ?>
+
+</body>
+</html>
